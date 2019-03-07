cb95b2ff9fb4d7986a4ef2bf820a4bdfe0fd729e
sindhuja <sindhuja@appranix.com>
2019-03-07 23:46:47 +0530

    Logic for check result completed and TODO mentioned

--- python_game.py
+++ python_game.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 from builtins import input
 import numpy
 import time
+from collections import defaultdict
 
 class PythonGame:
 
@@ -29,8 +30,7 @@ class PythonGame:
 
     def form_board(self):
 
-        # feed the  values manually
-
+        # feed the  values manually)
         self.board[0,0] = 1
         self.board[0,2] = 6
         self.board[0,5] = 2
@@ -102,31 +102,54 @@ class PythonGame:
                 print("|", end="")
 
 
-    def result(self):
-        for each in numpy.nditer(self.form_board()):
+    def result(self, updated_board):
+
+        row = 0
+        column = 0
+        for each in numpy.nditer(updated_board):
             if each == " ":
                 return False
-        # for each in numpy.nditer(self.form_board()):
+
+        while(True):
+            get_row_list = updated_board[row:row+1, 0:9]
+            get_column_list = updated_board[0:9, column:column+1]
+            if any([numpy.sum(get_row_list) != 45, numpy.unique(get_row_list).size != 9, numpy.where(numpy.logical_and(get_row_list>=1, get_row_list<=9))[1].size != 9, numpy.sum(get_column_list) != 45, numpy.unique(get_column_list).size != 9, numpy.where(numpy.logical_and(get_column_list>=1, get_column_list<=9))[1].size != 9]):
+                return False    
+            if row >= 8 and column >= 8:
+                break
+            row+=1
+            column+=1
+            break
+        return True
 
 
 play_game = PythonGame()
 
 print("Welcome !!! Let's play sudoko:")
 print("Board will be presented here:")
-time.sleep(2)
+time.sleep(1)
 play_game.print_board()
+
 print("Lets start the game. Fill the empty spaces with relevant values. To update the board specify the horizontal and vertical character: (eg : a,B,2)\n")
 while(True):
-    get_row = input().split(",")
-    print(get_row)
+    print("Get input : (row_number, column_number, value to be inserted)")
+    get_val = input().split(",")
+    if len(get_val) != 3:
+        print("Invalid Input. Try to provide input as mentioned: ( row_number, column_number, value to be inserted )")
+     
     play_game.print_board()
     print("Do you want to play further: ( Y | N ):")
     get_input = input()
-    if get_input != "Y" or get_input != "y":
+    if get_input != "Y" and get_input != "y":
         break
 
-check_result = play_game.result()
+check_result = play_game.result(play_game.form_board())
 if check_result == True:
     print("You Won!!!")
 else:
     print("Sorry you lost. Better luck next time")
+
+
+#TODO: Need to update the values of empty cells
+#TODO: Refactor board form process with defaultdict
+#TODO: Refactor print board process
